6dde17c75fdca5ebe99fdbf1146c3fe36bcc8cc0
thehenrikwoebel <henrik.woebel@gmail.com>
2024-08-25 01:40:32 +0200

    base path added

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import {
+    defineConfig
+  } from 'vite'
+  import vue from '@vitejs/plugin-vue'
+  import vuetify from '@vuetify/vite-plugin'
+  
+  const path = require('path')
+  
+  export default defineConfig({
+    plugins: [
+      vue(),
+  
+      vuetify({
+        autoImport: true,
+      }),
+    ],
+    define: {
+      'process.env': {}
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    base: './',
+  
+  })
